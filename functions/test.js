d2b5582513c1d7c046807ac4d3f9a0debae3ab0d
Leith-vdl <leith.green@dynamicplatform.com>
2024-10-14 11:08:19 +0100

    fix

--- functions/test.js
+++ functions/test.js
@@ -1,13 +1,3 @@
-export default async function handler(request) {
-    const jsonResponse = {
-        message: "Hello, Cloudflare Pages!",
-        status: "success",
-        timestamp: Date.now(),
-    };
-
-    return new Response(JSON.stringify(jsonResponse), {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-}
+export function onRequest(context) {
+    return new Response("Hello, world!")
+  }
