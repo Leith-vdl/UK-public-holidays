5cf60b9ec6e8754e063bc9225bb5a5b40a2db732
Leith-vdl <leith.green@dynamicplatform.com>
2024-10-16 13:16:33 +0100

    updated hello-liquid

--- functions/hello-liquid.js
+++ functions/hello-liquid.js
@@ -6,13 +6,16 @@ export async function onRequest(context) {
     //create a new instance to parse and render the template
     const engine = new Liquid();
     //selects the template and div respectively
-    const template = '<h3>{{ name | capitalize | prepend: "Hello "}}</h3> {{ foo }}'
-    const userAgent = context.request.headers.get('User-Agent')
+    const template = `
+    <h3>Hello {{ name | capitalize }}</h3>
+     <p>You are connected using the browser:  {{ foo }}.</p>
+     `
 
+    const userAgent = context.request.headers.get('User-Agent')
     try {
 
         //turn template into HTML
-        const html = await engine.parseAndRender(template, { name: 'Leith', foo: userAgent })
+        const html = await engine.parseAndRender(template, { name: 'zephan', foo: userAgent })
 
         return new Response(html, {
             headers: {
