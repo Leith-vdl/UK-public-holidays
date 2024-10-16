fc3c62f7bfdd96c2a9688696e3b14f881d8666a4
Leith-vdl <leith.green@dynamicplatform.com>
2024-10-16 16:47:39 +0100

    add browser headers to hello-liquid

--- functions/hello-liquid.js
+++ functions/hello-liquid.js
@@ -1,21 +1,74 @@
-
 import { Liquid } from 'liquidjs';
-
 
 export async function onRequest(context) {
     //create a new instance to parse and render the template
     const engine = new Liquid();
+
     //selects the template and div respectively
     const template = `
-    <h3>Hello {{ name | capitalize }}</h3>
-     <p>You are connected using the browser:  {{ foo }}.</p>
+    <h3>Hello {{ name | capitalize }}, here are all of your browser headers:</h3>
+    <p><b>Accept:</b> {{ accept }}</p>
+    <p><b>Accept-Encoding:</b> {{ accEncode }}</p>
+    <p><b>Accept-Language:</b> {{ accLang }}</p>
+    <p><b>Cache-Control:</b> {{ cacheCont }}</p>
+    <p><b>Connection:</b> {{ conn }}</p>
+    <p><b>Host:</b> {{ bHost }}</p>
+    <p><b>Sec-Ch-Ua:</b> {{ sCU }}</p>
+    <p><b>Sec-Ch-Ua-Mobile:</b> {{ sCUMobile }}</p>
+    <p><b>Sec-Ch-Ua-Platform:</b> {{ sCUPlat }}</p>
+    <p><b>Sec-Fetch-Dest:</b> {{ sFD }}</p>
+    <p><b>Sec-Fetch-Mode:</b> {{ sFM }}</p>
+    <p><b>Sec-Fetch-Site:</b> {{ sFS }}</p>
+    <p><b>Sec-Fetch-User:</b> {{ sFU }}</p>
+    <p><b>Upgrade-Insecure-Requests:</b>  {{ uIR }}</p>
+    <p><b>User-Agent:</b> {{ agent }}</p>
+    <h1>User Information:</h1>
+    <p><b>Name:</b> {{ queryName }}</p>
+    <p><b>Age:</b> {{ QueryAge }}</p>
      `
-
+    const browserAccept = context.request.headers.get('Accept')
+    const acceptEncoding = context.request.headers.get('Accept-Encoding')
+    const acceptlanguage = context.request.headers.get('Accept-Language')
+    const cacheControl = context.request.headers.get('Cache-Control')
+    const browserConnection = context.request.headers.get('Connection')
+    const browserHost = context.request.headers.get('Host')
+    const SecChuUa = context.request.headers.get('Sec-Ch-Ua')
+    const SecChuUaMobile = context.request.headers.get('Sec-Ch-Ua-Mobile')
+    const SecChuUaPlatform = context.request.headers.get('Sec-Ch-Ua-Platform')
+    const SecFetchDest = context.request.headers.get('Sec-Fetch-Dest')
+    const SecFetchMode = context.request.headers.get('Sec-Fetch-Mode')
+    const SecFetchSite = context.request.headers.get('Sec-Fetch-Site')
+    const SecFetchUser = context.request.headers.get('Sec-Fetch-User')
+    const upgradeInsecureRequests = context.request.headers.get('Upgrade-Insecure-Requests')
     const userAgent = context.request.headers.get('User-Agent')
-    try {
 
+    //parse query parameters
+    const url = new URL(context.request,url);
+    const queryName = url.searchParams.get('name' || 'Guest');
+    const queryAge = url.searchParams.get('Age' || 'Unknown');
+
+
+    try {
         //turn template into HTML
-        const html = await engine.parseAndRender(template, { name: 'zephan', foo: userAgent })
+        const html = await engine.parseAndRender(template, 
+            { name: 'leith', 
+                agent: userAgent, 
+                accept: browserAccept, 
+                accEncode: acceptEncoding,
+                accLang: acceptlanguage, 
+                cacheCont: cacheControl, 
+                conn: browserConnection, 
+                bHost: browserHost,
+                sCU: SecChuUa, 
+                sCUMobile: SecChuUaMobile, 
+                sCUPlat: SecChuUaPlatform, 
+                sFD: SecFetchDest,
+                sFM: SecFetchMode, 
+                sFS: SecFetchSite, 
+                sFU: SecFetchUser, 
+                uIR: upgradeInsecureRequests,
+                queryName, queryAge
+             })
 
         return new Response(html, {
             headers: {
---
+++ tasks.md
@@ -0,0 +1,5 @@
+TO DO:
+- [✔️] wrangler local dev
+- [✔️] add all the browser headers to hello-liquid
+- [?] add cf headers
+- [-] add request variables https://en.wikipedia.org/wiki/Query_string
