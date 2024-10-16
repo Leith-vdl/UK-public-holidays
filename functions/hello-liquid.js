c58c30a9e3673406a5879eff1679eae956edc796
Leith-vdl <leith.green@dynamicplatform.com>
2024-10-16 12:40:24 +0100

    fixed worker function to run correctly

--- functions/hello-liquid.js
+++ functions/hello-liquid.js
@@ -1,31 +1,35 @@
 //imports the liquid templating engine from a CDN (Content Delivery NEtwork)
 import liquidjs from 'https://cdn.jsdelivr.net/npm/liquidjs@10.17.0/+esm';
 
-//extract the Liquid class that has been imported above
-const { Liquid } = liquidjs;
-
-//create a new instance to parse and render the template
-const engine = new Liquid();
-//selects the template and div respectively
-const template = document.querySelector('[type="text/template"]')
-const result = document.querySelector('#result')
 
 
-//from the LiquidJS library, provides methods (parseAndRender) and handles data
-engine
-    //object with variables to be passed to the template, promise that returns HTML wehen resolved
-	.parseAndRender(template.innerHTML, {name: 'liquid'})
-	.then(html => {
-        result.innerHTML = html;
+export async function onRequest(context) {
+    //extract the Liquid class that has been imported above
+    const { Liquid } = liquidjs;
+
+    //create a new instance to parse and render the template
+    const engine = new Liquid();
+    //selects the template and div respectively
+    const template = '<h3>{{ name | capitalize | prepend: "Hello "}}</h3>'
+
+    try {
+
+        //turn template into HTML
+        const html = await engine.parseAndRender(template, { name: 'Leith' })
+
         return new Response(html, {
             headers: {
-              'Content-Type': 'text/html',
-              // 'Access-Control-Allow-Origin': request.headers.get('origin') ...not required becuase it's a function same domain
-        }
-    });
-})
-//error handling logs to console if called
-.catch(error => {
-    console.error( 'Error rendering template:', error);
-});
-    
+                'Content-Type': 'text/html',
+
+            },
+        });
+    } catch (e) {
+        return new Response(`error: ${e}`, {
+            status: 500,
+            headers: {
+                'Content-Type': 'text/plain',
+
+            },
+        });
+    }
+} 
